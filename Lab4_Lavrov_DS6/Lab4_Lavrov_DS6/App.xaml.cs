a25f16d900062eaee0ef58f569440a7aa71da522
Vlad Lavrov <chess2335@ukr.net>
2024-04-04 21:45:32 +0200

    Лаб4 - калькулятор з використанням Relative Layout

---
+++ Lab4_Lavrov_DS6/Lab4_Lavrov_DS6/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab4_Lavrov_DS6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
